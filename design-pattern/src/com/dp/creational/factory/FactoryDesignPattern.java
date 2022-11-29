e517f80b67cd505a47ec3de3f9af4a0d85ead8ca
ajaykumartiwari <tiwariajay621@gmail.com>
2022-11-29 21:08:07 +0530

    test

--- design-pattern/src/com/dp/creational/factory/FactoryDesignPattern.java
+++ design-pattern/src/com/dp/creational/factory/FactoryDesignPattern.java
@@ -9,6 +9,7 @@ public class FactoryDesignPattern {
 	 * for creating object and let the subclasses decide which class to instantiate.
 	 * 
 	 * It is one of the best ways to create an object where object creation logic is hidden from the client
+	 * 
 	 */
 	public static void main(String[] args) {
 		NotificationFactory notificationFactory = new NotificationFactory();
